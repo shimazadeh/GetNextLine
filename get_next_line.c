3026dfb832fd5180b42cf8067ac8da612847ecd1
Shima HABIBOLLAHZADEH <shabibol@e1r6p3.42.fr>
2022-04-05 23:58:38 +0200

    1st garbage

---
+++ get_next_line.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shabibol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/05 20:55:11 by shabibol          #+#    #+#             */
+/*   Updated: 2022/04/05 23:57:48 by shabibol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	ft_strlen(char *s1)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*dst;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	dst = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!dst)
+		return (0);
+	while (s1[i] != '\0')
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+		dst[i++] = s2[j++];
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strchr_boolean(char *s1)
+{
+	while (*s1 && *s1 != '\0')
+		s1++;
+	if (*s1 == '\0')
+		return (s1);
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[BUFFER_SIZE];
+	char		*line;
+	char		*res;
+	int			byte_read;
+
+	if (!fd)
+		return (0);
+	byte_read = 0;
+	while (BUFFER_SIZE > 0)
+	{
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		while (ft_strchr_boolean(res) != 
+				res = ft_strjoin(res, buffer);
+		
+	}
+	
+	return (line);
+}
